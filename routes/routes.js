afaeeebdfc73cc726b72945a6ee6c0b227f411cb
cybercoder-naj <cybercoder.nishant@gmail.com>
2022-11-09 23:17:48 +0000

    Added more boilerplate

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,18 @@
 const router = require('express').Router()
+const Card = require('../models/Card')
 
 router.get('/', (req, res) => res.send("Working"))
+
+router.get('/cards', async (req, res) => {
+  // Code here
+})
+
+router.post('/cards', async (req, res) => {
+  // Code here
+})
+
+router.put('/cards/:id', async (req, res) => {
+  // Code here
+})
 
 module.exports = router
