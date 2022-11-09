dce860feb9ec6b77ec32e6afcd650d19d1898d5c
cybercoder-naj <cybercoder.nishant@gmail.com>
2022-11-09 23:47:17 +0000

    Added next parameter

--- routes/routes.js
+++ routes/routes.js
@@ -3,23 +3,12 @@ const Card = require('../models/Card')
 
 router.get('/', (req, res) => res.send("Working"))
 
-router.get('/cards', async (req, res) => {
+router.get('/cards', async (req, res, next) => {
   // Code here
 })
 
 router.post('/cards', async (req, res, next) => {
-  console.log(req.body)
-  const {content, category} = req.body
-  if (!content || !category) {
-    res.status(401)
-    return next(new Error("Bad request body"))
-  }
-  try {
-    const card = await Card.create(req.body)
-    res.json(card)
-  } catch(err) {
-    return next(err)
-  }
+  // Code here
 })
 
 router.put('/cards/:id', async (req, res, next) => {
