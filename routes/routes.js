5dd7c5210fd92de32e20a9767d814d0ad5983016
cybercoder-naj <cybercoder.nishant@gmail.com>
2022-11-09 23:46:39 +0000

    Added next parameter

--- routes/routes.js
+++ routes/routes.js
@@ -7,11 +7,22 @@ router.get('/cards', async (req, res) => {
   // Code here
 })
 
-router.post('/cards', async (req, res) => {
-  // Code here
+router.post('/cards', async (req, res, next) => {
+  console.log(req.body)
+  const {content, category} = req.body
+  if (!content || !category) {
+    res.status(401)
+    return next(new Error("Bad request body"))
+  }
+  try {
+    const card = await Card.create(req.body)
+    res.json(card)
+  } catch(err) {
+    return next(err)
+  }
 })
 
-router.put('/cards/:id', async (req, res) => {
+router.put('/cards/:id', async (req, res, next) => {
   // Code here
 })
 
